d20c0783a6c1cde555537e6e27f96ce5d5c4ebb1
Garth Egbert <garth.egbert@stantec.com>
2023-02-22 19:26:57 +0000

    correct resource group name

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/garth-training/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "hashikat-rg"
+  resource_group_name = "garth-workshop"
 }
